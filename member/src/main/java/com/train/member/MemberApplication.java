092eed3dff521d4b128d84160c521a8aaf45422f
... <37700859+453594848@users.noreply.github.com>
2024-07-17 01:42:16 +0800

    集成mybatis,创建member模块表

--- member/src/main/java/com/train/member/MemberApplication.java
+++ member/src/main/java/com/train/member/MemberApplication.java
@@ -1,5 +1,6 @@
 package com.train.member;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -10,7 +11,7 @@ import org.springframework.core.env.Environment;
 @SpringBootApplication
 //@EnableAspectJAutoProxy
 @ComponentScan("com.train")//扫描其他模块train下的包
-//@MapperScan("com.member.mapper")
+@MapperScan("com.train.member.mapper")
 public class MemberApplication {
 
     private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
