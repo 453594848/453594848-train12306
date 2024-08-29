bb8b300220c38e8e72002eefb21e92a0cefe32b9
... <37700859+453594848@users.noreply.github.com>
2024-08-29 23:36:53 +0800

    feign测试

--- business/src/main/java/com/train/business/controller/TestController.java
+++ business/src/main/java/com/train/business/controller/TestController.java
@@ -1,16 +1,21 @@
 package com.train.business.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class TestController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
+
     @GetMapping("/hello")
     public String hello() {
+        LOG.info("batch hello");
         return "Hello World! Business!";
     }
-
 
 
 }
