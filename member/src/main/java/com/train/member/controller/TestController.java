0d8278fe9f29afa9fd1f3449558122d95637d784
... <37700859+453594848@users.noreply.github.com>
2024-09-04 14:07:30 +0800

    集成nacos实现服务注册

--- member/src/main/java/com/train/member/controller/TestController.java
+++ member/src/main/java/com/train/member/controller/TestController.java
@@ -1,13 +1,19 @@
 package com.train.member.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @Autowired
+    Environment env;
     @GetMapping("/hello")
     public String hello() {
-        return "hello!!";
+        String property = env.getProperty("server.port");
+        return "hello!!"+property;
     }
 
 }
